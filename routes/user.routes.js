5837f3e3850acaf401a18ed5c9d80f0911cb69c0
Szymon Zygadliński <szygadlinski@gmail.com>
2021-05-31 01:40:38 +0200

    Add pages Profile & Settings, change permission settings

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  res.render(req.user ? 'logged' : 'noPermission');
+});
+
+router.get('/profile', (req, res) => {
+  res.render(req.user ? 'profile' : 'noPermission');
+});
+
+router.get('/profile/settings', (req, res) => {
+  res.render(req.user ? 'settings' : 'noPermission');
 });
 
 router.get('/no-permission', (req, res) => {
