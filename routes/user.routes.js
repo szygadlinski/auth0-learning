dc693fbb25f19c2170dfc9d1873a009c8b4c0434
Szymon Zygadliński <szygadlinski@gmail.com>
2021-05-31 01:55:27 +0200

    Add middleware isLogged

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,16 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/logged', (req, res) => {
-  res.render(req.user ? 'logged' : 'noPermission');
+const isLogged = (req, res, next) => {
+  req.user ? next() : res.redirect('/user/no-permission');
+};
+
+router.get('/logged', isLogged, (req, res) => {
+  res.render('logged');
 });
 
-router.get('/profile', (req, res) => {
-  res.render(req.user ? 'profile' : 'noPermission');
+router.get('/profile', isLogged, (req, res) => {
+  res.render('profile');
 });
 
-router.get('/profile/settings', (req, res) => {
-  res.render(req.user ? 'settings' : 'noPermission');
+router.get('/profile/settings', isLogged, (req, res) => {
+  res.render('settings');
 });
 
 router.get('/no-permission', (req, res) => {
